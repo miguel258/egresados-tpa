128d612048e3cf1ee9440a0790af38b7a58938d7
fmauricios <mauricioserf@hotmail.com>
2015-07-06 10:45:43 -0500

    Add styles

--- Loggin.php
+++ Loggin.php
@@ -2,19 +2,17 @@
 <html>
 <head>
 	<title >BIENVENIDO</title>
-
+	<link rel="stylesheet" href="./css/style.css">
 </head><header>BIENVENIDO</header><br>
 <body>
-<form name="F1" method="POST" action="ValidaAcceso.php" ><!--funcion que procesara los datos enviados desde el formulario-->
-USUARIO<input type="TEXT" name="User"><br><br>
-CLAVE<input type="TEXT" name="Pass" >
+	<form name="F1" method="POST" action="ValidaAcceso.php" ><!--funcion que procesara los datos enviados desde el formulario-->
+	USUARIO<input type="TEXT" name="User"><br><br>
+	CLAVE<input type="TEXT" name="Pass" >
 
-<input type="button" value="Registrarse" onclick="Validar() " />
-<!-- <button type="submit" name="btnRegistro" onclick="Validar()"> Registrarse</button> -->
-</form>
-
+	<input type="button" value="Registrarse" onclick="Validar() " />
+	<!-- <button type="submit" name="btnRegistro" onclick="Validar()"> Registrarse</button> -->
+	</form>
  	<div id="Resultado">
-
 	</div>
 	<script type="text/javascript" src="Uno.js"></script>
 </body>
